151529207601ac1975029e14fe1a0a43b39bdfab
ankitashrma <s.ankita3124@gmail.com>
2018-08-28 15:36:39 +0545

    seeders added

---
+++ src/seeds/insert_into_pages.js
@@ -0,0 +1,65 @@
+/**
+ * Seed users table.
+ *
+ * @param  {object} knex
+ * @param  {object} Promise
+ * @return {Promise}
+ */
+export function seed(knex, Promise) {
+  return knex('pages')
+    .del()
+    .then(() => {
+      return Promise.all([
+        knex('pages').insert([
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 3,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Login',
+            metadata_id: 3,
+            path: '/home',
+
+            referrer: '/home',
+            search: 'howto',
+            title: 'Login',
+
+            url: 'www.myurl.com/login',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Myapp',
+            metadata_id: 3,
+            path: '/',
+
+            referrer: '/home',
+            search: 'Myapp',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 3,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+        ]),
+      ]);
+    });
+}
