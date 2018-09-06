f91363efbb36ff9073dfd77a87123ed011690584
ankitashrma <s.ankita3124@gmail.com>
2018-09-06 12:52:09 +0545

    added seeds

--- src/seeds/insert_into_pages.js
+++ src/seeds/insert_into_pages.js
@@ -59,6 +59,102 @@ export function seed(knex, Promise) {
 
             url: 'www.myurl.com',
           },
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 10,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Login',
+            metadata_id: 10,
+            path: '/home',
+
+            referrer: '/home',
+            search: 'howto',
+            title: 'Login',
+
+            url: 'www.myurl.com/login',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Myapp',
+            metadata_id: 10,
+            path: '/',
+
+            referrer: '/home',
+            search: 'Myapp',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 10,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 15,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Login',
+            metadata_id: 15,
+            path: '/home',
+
+            referrer: '/home',
+            search: 'howto',
+            title: 'Login',
+
+            url: 'www.myurl.com/login',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Myapp',
+            metadata_id: 15,
+            path: '/',
+
+            referrer: '/home',
+            search: 'Myapp',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
+          {
+            updated_at: new Date(),
+            name: 'Home',
+            metadata_id: 15,
+            path: '/',
+
+            referrer: '/',
+            search: 'home',
+            title: 'Home',
+
+            url: 'www.myurl.com',
+          },
         ]),
       ]);
     });
